03a5c5094960c6c43e61042e6d11866aaa033a93
bcssov <nxsovereign@gmail.com>
2022-08-13 02:09:10 +0200

    All this trouble just to remove this silly limit

--- LiteDB/Engine/Services/DataService.cs
+++ LiteDB/Engine/Services/DataService.cs
@@ -30,7 +30,7 @@ namespace LiteDB.Engine
         {
             var bytesLeft = doc.GetBytesCount(true);
 
-            if (bytesLeft > MAX_DOCUMENT_SIZE) throw new LiteException(0, "Document size exceed {0} limit", MAX_DOCUMENT_SIZE);
+            //if (bytesLeft > MAX_DOCUMENT_SIZE) throw new LiteException(0, "Document size exceed {0} limit", MAX_DOCUMENT_SIZE);
 
             var firstBlock = PageAddress.Empty;
 
@@ -81,7 +81,7 @@ namespace LiteDB.Engine
         {
             var bytesLeft = doc.GetBytesCount(true);
 
-            if (bytesLeft > MAX_DOCUMENT_SIZE) throw new LiteException(0, "Document size exceed {0} limit", MAX_DOCUMENT_SIZE);
+            //if (bytesLeft > MAX_DOCUMENT_SIZE) throw new LiteException(0, "Document size exceed {0} limit", MAX_DOCUMENT_SIZE);
 
             DataBlock lastBlock = null;
             var updateAddress = blockAddress;
